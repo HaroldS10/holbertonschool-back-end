68fc37866d510cf919cbe06667cbef109828b3d6
HaroldS10 <haroldsanty13@gmail.com>
2023-12-21 16:16:49 -0500

    update correction for API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
         'Employee {} is done with tasks({}/{}):'
         .format(user['name'], len(completed), all_todos), file=sys.stdout)
     for todo_finish in completed:
-        print(todo_finish['title'], file=sys.stdout)
+        print('\t {}' ,format(todo_finish['title']), file=sys.stdout)
